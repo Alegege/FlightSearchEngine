b7c77df6fae41f2d6431213ea4da3b004a8b542a
Alejandro Garcia <alejandrogg84@gmail.com>
2018-09-06 08:52:41 +0200

    Initial commit

---
+++ FlightSearchEngine/FlightSearchEngine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlightSearchEngine
+//
+//  Created by Alejandro Garcia on 5/9/18.
+//  Copyright © 2018 AGG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
